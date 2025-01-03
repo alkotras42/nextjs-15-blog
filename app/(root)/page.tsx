579a232ef0530fe08ed7ad9a08e3798ec36edefe
alkotras42 <kirill.stogniy@mail.ru>
2025-01-04 00:46:16 +0300

    Add header with SSR form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,18 @@
-export default function Home() {
-  return (
-    <h1 className='text-3xl'>Home</h1>
+import SearchForm from '../../components/SearchForm';
 
+export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
+  const query = (await searchParams).query;
+  return (
+    <>
+      <section className='pink_container'>
+        <h1 className='heading'>
+          Post Your Ideas, <br /> Share Your Thoughts
+        </h1>
+        <p className='sub-heading !max-w-3xl'>
+          Submit your startup ideas, share your thoughts, and connect with the world.
+        </p>
+        <SearchForm query={query}></SearchForm>
+      </section>
+    </>
   );
 }
